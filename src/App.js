6162d337400fee38553afae19148d48793e1a395
Nigel_Fernandes <nfern@uw.edu>
2021-02-01 23:42:00 -0800

    first

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {useState} from 'react';
+import './App.css';
+import TextInput from './TextInput';
+import Message from './Message'
+
+function App() {
+  const [messages,setMessages] = useState([])
+
+  return <div className="App">
+
+    <header className="header">
+      <div className="logo" />
+      CHATTER
+    </header>
+
+    <main className="messages">
+      {messages.map((m,i)=> {
+        return <Message key={i} {...m} />
+      })}
+    </main>
+
+    <TextInput
+      send={(t)=> setMessages( [{text:t}, ...messages] )}
+    />
+
+  </div>
+}
+
+export default App;
---
+++ src/TextInput.js
@@ -0,0 +1,34 @@
+
+import {useState} from 'react'
+import { AiOutlineSend } from "react-icons/ai";
+
+function TextInput(props) {
+  const [text, setText] = useState('')
+
+  function sendMessage(){
+    if(text==='') return // skip the function
+    props.send(text)
+    setText('')
+  }
+  function keyPressed(e){
+    if(e.key==='Enter') {
+      sendMessage()
+    }
+  }
+
+  return <footer className="text-input">
+    <input className="input"
+      placeholder="Write your message"
+      value={text}
+      onChange={e=> setText(e.target.value)}
+      onKeyPress={keyPressed}
+    />
+    <button onClick={sendMessage} disabled={!text} className="send-button">
+      <AiOutlineSend title="send"
+        style={{minWidth:'0.7rem',marginLeft:2}}
+      />
+    </button>
+  </footer>
+}
+
+export default TextInput
