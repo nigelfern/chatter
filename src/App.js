c1da924c5f3e320c994b121762d8d9e1f2356066
Nigel_Fernandes <nfern@uw.edu>
2021-02-08 23:08:57 -0800

    A5

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import {useState} from 'react';
 import './App.css';
 import TextInput from './TextInput';
 import Message from './Message'
+import NamePicker from './NamePicker'
+import {db, useDB} from './db'
 
 function App() {
-  const [messages,setMessages] = useState([])
+  const messages = useDB()
+  const [username,setUsername] = useState(
+    localStorage.getItem('username') || ''
+  )
 
+  console.log(messages)
   return <div className="App">
-
     <header className="header">
       <div className="logo" />
       CHATTER
+      <NamePicker saveName={setUsername} />
     </header>
 
     <main className="messages">
-      {messages.map((m,i)=> {
-        return <Message key={i} {...m} />
+      {messages.map((msg,i)=> {
+        const isMe = msg.name===username
+        return <Message key={i} {...msg} isMe={isMe} />
       })}
     </main>
 
     <TextInput
-      send={(t)=> setMessages( [{text:t}, ...messages] )}
+      send={(t)=> db.send({text:t, name:username, date:new Date()})}
     />
 
   </div>
