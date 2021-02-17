6a9ef3a65a00d0ec38bb8fd399b15bcb927c4ce4
Nigel_Fernandes <nfern@uw.edu>
2021-02-16 23:37:17 -0800

    prepared for deployment

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import TextInput from './TextInput';
 import Message from './Message'
 import NamePicker from './NamePicker'
 import {db, useDB} from './db'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  const messages = useDB()
+export default function Wrap() {
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/:room" component={App} />
+    </Switch>
+  </BrowserRouter>
+}
+
+function App(props) {
+  const room = props.match.params.room || 'home'
+
+  const messages = useDB(room)
   const [username,setUsername] = useState(
     localStorage.getItem('username') || ''
   )
 
-  console.log(messages)
+  // console.log(messages)
   return <div className="App">
+
     <header className="header">
       <div className="logo" />
       CHATTER
@@ -27,10 +40,8 @@ function App() {
     </main>
 
     <TextInput
-      send={(t)=> db.send({text:t, name:username, date:new Date()})}
+      send={(t)=> db.send({text:t, name:username, date:new Date(), room})}
     />
 
   </div>
 }
-
-export default App;
